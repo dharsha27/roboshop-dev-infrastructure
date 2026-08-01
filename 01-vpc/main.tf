d7e35a5c7c213c8c2419c248b3b4547b3d7d14ab
dharsha27 <dharshavardhan27@gmail.com>
2026-08-01 22:52:11 +0530

    Initial commit with proper root gitignore

---
+++ 01-vpc/main.tf
@@ -0,0 +1,9 @@
+module "vpc"{
+
+    # source="../tf-aws-vpc"
+    source="git::https://github.com/dharsha27/tf-aws-vpc.git?ref=main"
+    project=var.project
+    environment=var.environment
+    is_peering_required=false
+
+}
