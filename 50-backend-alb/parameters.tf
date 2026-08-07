a32996377963b1e6acc5f0488c873eaf9310c71c
dharsha27 <dharshavardhan27@gmail.com>
2026-08-07 17:49:32 +0530

    done

--- 50-backend-alb/parameters.tf
+++ 50-backend-alb/parameters.tf
@@ -1,7 +1,7 @@
-# resource "aws_ssm_parameter" "backend_alb_listener_arn" {
+resource "aws_ssm_parameter" "backend_alb_listener_arn" {
  
-#   name  = "/${var.project}/${var.environment}/backend_alb_listener_arn" # /roboshop/dev/backend_alb_sg_id, /roboshop/dev/mongodb_sg_id
-#   type  = "String"
-#   value = aws_lb_listener.http.arn
-#   overwrite = true
-# }
+  name  = "/${var.project}/${var.environment}/backend_alb_listener_arn" # /roboshop/dev/backend_alb_sg_id, /roboshop/dev/mongodb_sg_id
+  type  = "String"
+  value = aws_lb_listener.http.arn
+  overwrite = true
+}
