4dd7b82adf40e7826f924dd6820343b7d315196c
dharsha27 <dharshavardhan27@gmail.com>
2026-08-07 21:13:18 +0530

    done

---
+++ 70-acm/variable.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "devopspractice.online"
+}
+
+variable "zone_id" {
+    default = "Z02304293I0EIMA6V7PSK"
+}  
